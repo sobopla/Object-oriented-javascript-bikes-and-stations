d377d225d7016d2e15f6f67e4927b021d1a15265
Catherine Jean <catherinejeanbean@gmail.com>
2017-07-31 18:45:44 -0500

    Fuuuuuuuuuuuuuuuuuuuun station add

--- src/station.js
+++ src/station.js
@@ -1,1 +1,29 @@
-var Station = function() {}
+var Station = function(name, location, bikes =[],) {
+	this.name = name;
+	this.location = location;
+	this.bikes = bikes;
+}
+
+Station.prototype.take = function(bike) {
+
+  this.bikes.push(bike);
+};
+
+Station.prototype.empty = function() {
+	if (this.bikes.length === 0) {
+    return true;
+  }
+  else {
+    return false;
+  } 
+};
+
+
+Station.prototype.newestBike = function() {
+	// if (this.bikes.length === 0) {
+ //    return true;
+ //  }
+ //  else {
+ //    return false;
+ //  } 
+};
